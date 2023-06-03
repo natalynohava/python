cf5bb3951decb745277d7cebdc308564c81df2f2
Angie Nohava <angienoahav03@gmail.com>
2023-06-02 23:03:35 -0500

    commit

---
+++ operaciones.py
@@ -0,0 +1,13 @@
+#4 operaciones basicas
+num1 = 2
+num2 = 3
+
+multiplicacion = num1*num2
+division = num1/num2
+suma = num1+num2
+resta = num1-num2
+
+print("la multiplicacion es: ",multiplicacion)
+print("la division es: ",division)
+print("la suma es: ",suma)
+print("la resta es: ",resta)
